ac96836d63681279882cbbdc6aafc4b373081b65
Alexandru Ionica <alexandru.ionica@student.upt.ro>
2023-05-25 07:54:22 +0300

    fix benchmarks

--- cpuworker.cpp
+++ cpuworker.cpp
@@ -9,17 +9,32 @@ cpuWorker::cpuWorker() : QObject()
 QString cpuWorker::runBenchmark()
 {
     QString result;
-    auto* bench = new benchmark::cpu::whetstoneCpuBenchmark();
-    bench->initialize(INT32_MAX);
-    bench->setNrThreads(16);
-    bench->runAbsolute(true);
+    auto* bench1 = new benchmark::cpu::whetstoneCpuBenchmark();
+    bench1->initialize(INT32_MAX);
+    bench1->setNrThreads(16);
+    bench1->runAbsolute(true);
+    result += "Wetstone\n";
     result += "Multithreaded performace: ";
-    result += QString::number(bench->getResult());
+    result += QString::number(bench1->getResult());
     result += "\n";
-    bench->setNrThreads(1);
-    bench->runAbsolute(false);
+    bench1->setNrThreads(1);
+    bench1->runAbsolute(false);
     result += "Singlethreaded performance: ";
-    result += QString::number(bench->getResult());
+    result += QString::number(bench1->getResult());
+    result += "\n\n";
+    auto* bench2 = new bench::Cpu::CpuMatrixMultiplication();
+    bench2->setMatrixSize(2000);
+    bench2->setNumThreads(6);
+    bench2->initialize();
+    bench2->runAbsoluteBenchmark(true);
+    result += "Matrix Multiplication\n";
+    result += "Multithreaded performace: ";
+    result += QString::number(bench2->getResult());
+    result += "\n";
+    result += "Singlethreaded performance: ";
+    bench2->runAbsoluteBenchmark(false);
+    result += QString::number(bench2->getResult());
+    delete bench2;
 
     return result;
 }
--- hddworker.cpp
+++ hddworker.cpp
@@ -1,5 +1,6 @@
 #include "hddworker.h"
 #include "benchmark/hdd/hddWriteBenchmark.h"
+#include "benchmark/hdd/hddReadBenchmark.h"
 
 namespace Benchmarking {
 
@@ -11,9 +12,29 @@ hddWorker::hddWorker() : QObject()
 QString hddWorker::runBenchmark()
 {
     QString result;
-    auto* bench = new benchmark::hdd::HDDWriteSpeedBenchmark();
-    bench->run("fs");
-    result += QString::number(bench->getResult());
+    auto* bench1 = new benchmark::hdd::HDDWriteSpeedBenchmark;
+    bench1->setMinIndex(1);
+    bench1->setMaxIndex(8);
+    bench1->setPrefix("data-files/data");
+    bench1->setSuffix(".dat");
+    bench1->run("fs");
+    result+= "Fixed size write speed: ";
+    result += QString::number(bench1->getResult()) + " MB/s\n";
+    result += "\n";
+    bench1->run("fb");
+    result+= "Fixed buffer write speed: ";
+    result += QString::number(bench1->getResult()) + " MB/s\n";
+    result += "\n";
+    delete bench1;
+    auto* bench2 = new Bench::HDD::HddReadBenchmark;
+    bench2->initialize(1024LL*1024LL*1024LL*16);
+    bench2->setItterations(1);
+    bench2->setBufferSize(1024*4);
+    bench2->setFileName("read-data.dat");
+    bench2->run();
+    result += "Read speed: ";
+    result += QString::number(bench2->getResult()) + " MB/s\n";
+    delete bench2;
     return result;
 }
 
